40e7c1c647fc50c48e7440b6ec251782b74b9da6
ivNetAdmin <bp@ivnet.co.uk>
2023-10-28 01:16:52 +0100

    Added Autofac dependency injection

---
+++ Services/Interfaces/IDataService.cs
@@ -0,0 +1,10 @@
+﻿using ivnet.club.services.api.Models;
+using System.Collections.Generic;
+
+namespace ivnet.club.services.api.Services.Interfaces
+{
+    public interface IDataService
+    {
+        List<RinkBooking> GetRinkBookings();
+    }
+}
